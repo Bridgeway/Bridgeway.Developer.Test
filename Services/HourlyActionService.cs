4f43b718193ea6b5052ce4c19141a7a4b5513ff4
Robert Meakin <meakin.robert@gmail.com>
2019-02-17 19:53:04 +0000

    removed notes, changed WriteLine to user logger.

--- Services/HourlyActionService.cs
+++ Services/HourlyActionService.cs
@@ -22,18 +22,11 @@ namespace Bridgeway.Developer.Test.Services {
         }
 
         private void PerformAction(object state) {
-            // This will get called once an hour.
-            // Something should go here that will check the time of day and perform an action if we want it to.
-            // We want to be be able to run new code on certain days at a certain hour.
-            // For example, on Mondays and Wednesdays, any time between 1400 and 1500.
-            // The way we implement this needs to be extendable, as in, we can easily add new actions as needed.
-            // It should be object oriented and follow the rule of being open-closed. Open for extension but closed for change.
-            // We might want to call these things: ScheduledActions.
             _exampleService.WriteLineToLog();
         }
 
         public void Dispose() {
-            System.Console.WriteLine("HourlyActionService Disposed");
+            _logger.LogInformation("HourlyActionService Disposed {0}", DateTime.Now);
             _timer?.Dispose();
         }
 
